27dbddd7bf971db4fde83f36b66096ba3aa3016f
Deepak Nallajerla <deepaknbk@gmail.com>
2020-12-15 21:31:23 -0600

    working version of directors code

--- Day4-6/directors.py
+++ Day4-6/directors.py
@@ -1,8 +1,9 @@
 import csv
 from collections import defaultdict, namedtuple
+import pprint
 
 MOVIE_DATA = 'movie_metadata.csv'
-NUM_TOP_DIRECTORS = 20
+NUM_TOP_DIRECTORS =10
 MIN_MOVIES = 4
 MIN_YEAR = 1960
 
@@ -22,18 +23,30 @@ def get_movies_by_director():
             title_score=row['imdb_score']
             m=Movie(title=title_movie,year=title_year,score=title_score)
             directors[director].append(m)
-        return directors
-    pass
+    return directors
 
 
 def get_average_scores(directors):
     '''Filter directors with < MIN_MOVIES and calculate averge score'''
-    pass
+    nominated_directors = {}
+    i=0
+    for director, movies in directors.items():
+        if len(movies) > MIN_MOVIES:
+            #avg_score=10
+            #pprint(director,movies)
+            nominated_directors[(director, _calc_mean(movies))] = movies
+    return nominated_directors
+
 
 
 def _calc_mean(movies):
     '''Helper method to calculate mean of list of Movie namedtuples'''
-    pass
+    avg_score=0
+    #print('start')
+    for row in movies:
+        avg_score +=float(row.score)
+    #print(avg_score/len(movies))
+    return round(avg_score / len(movies),1)
 
 
 def print_results(directors):
@@ -43,19 +56,23 @@ def print_results(directors):
     fmt_director_entry = '{counter}. {director:<52} {avg}'
     fmt_movie_entry = '{year}] {title:<50} {score}'
     sep_line = '-' * 60
+    report = sorted(directors.items(), key=lambda x: float(x[0][1]), reverse=True)
+    for i in range(NUM_TOP_DIRECTORS):
+        print(fmt_director_entry.format(counter=i + 1, director=report[i][0][0], avg=report[i][0][1]))
+        print(sep_line)
+        for movie in report[i][1]:
+            #print(movie)
+            print(fmt_movie_entry.format(year=movie.year, title=movie.title, score=movie.score))
+        print()
 
 
 def main():
     '''This is a template, feel free to structure your code differently.
     We wrote some tests based on our solution: test_directors.py'''
+    pp = pprint.PrettyPrinter()
     directors = get_movies_by_director()
-    i = 0
-    for row in directors.items():
-        if i<10:
-            i=i+1
-            print(row)
-        else: continue
     directors = get_average_scores(directors)
+    #pp.pprint(directors)
     print_results(directors)
 
 
