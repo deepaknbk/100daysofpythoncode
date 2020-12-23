6b62811ae1003a56b04b82f607beb0023fb2ad0f
Deepak Nallajerla <deepaknbk@gmail.com>
2020-12-22 21:23:12 -0600

    Update 15rps.py

--- Day13-15-text-games/15rps.py
+++ Day13-15-text-games/15rps.py
@@ -8,13 +8,11 @@ def read_rolls():
 
 
 def read_roll(row: dict):
-    print("inside read roll:",row)
-    print(row['Attacker'])
     name = row['Attacker']
-    print(row['Attacker'])
+
     del row['Attacker']
     del row[name]
-    print("inside read roll2:", row)
+
     print("Roll: {}".format(name))
     for k in row.keys():
         can_defeat = row[k].strip().lower() == 'win'
