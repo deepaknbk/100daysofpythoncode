d15d0e1f6423b8e3a032a97937051659a8add63a
Deepak Nallajerla <deepaknbk@gmail.com>
2020-12-26 23:35:19 -0600

    itertools permitation and combinations

---
+++ Day19-21-itertools/permitation_combination_demo.py
@@ -0,0 +1,9 @@
+from itertools import permutations,combinations
+
+family='Deepak Swapna Aashri'.split()
+
+print(family)
+
+print(list(combinations(family,2)))
+
+print(list(permutations(family,2)))
