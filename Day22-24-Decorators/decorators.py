55af65796fe865782491303ded3e28df352e76ae
Deepak Nallajerla <deepaknbk@gmail.com>
2021-01-02 10:38:07 -0600

    Bite 22. Write a decorator with argument

--- Day22-24-Decorators/decorators.py
+++ Day22-24-Decorators/decorators.py
@@ -8,3 +8,21 @@ def decorator(func):
         # Do something after
         return value
     return wrapper_decorator
+
+def make_html(tag):
+    def decorator_make_html(func):
+        @functools.wraps(func)
+        def wrapper_make_html(*args, **kwargs):
+            # Do something before using arg_1, ...
+            value = func(*args, **kwargs)
+            # Do something after using arg_1, ...
+            return f'<{tag}>{value}</{tag}>'
+        return wrapper_make_html
+    return decorator_make_html
+
+@make_html('p')
+@make_html('strong')
+def get_text(text='I code with PyBites'):
+    return text
+
+#print(get_text())
